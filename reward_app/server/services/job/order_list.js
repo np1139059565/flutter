6f3e1794e5e1ef3f797b18d5ba099ff472d048aa
np1139059565 <3375863936lcy>
2022-12-20 20:11:37 +0800

    data add rand

--- reward_app/server/services/job/order_list.js
+++ reward_app/server/services/job/order_list.js
@@ -19,11 +19,12 @@ function _get(request, response) {
     console.info(request.url, params);
 
     const statusArr = ['未提交', '审核中', '已通过', '未通过'];
-    let orderTimeGeo=''
+    let orderTimeGeo='';
+    const thisUseSecondsGeo='UNIX_TIMESTAMP(now())-UNIX_TIMESTAMP(A.order_time)';
     if (params.status < statusArr.length && params.status >= 0) {
         params.status = statusArr[params.status];
         //时间直接相减是有问题的,需要转成整数https://www.cnblogs.com/zhenxing/p/16355843.html
-        orderTimeGeo='AND UNIX_TIMESTAMP(now())-UNIX_TIMESTAMP(A.order_time)<B.max_used_seconds'
+        orderTimeGeo='AND '+thisUseSecondsGeo+'<B.max_used_seconds';
     } else {
         params.status = statusArr.join('\',\'');
     }
@@ -34,7 +35,7 @@ function _get(request, response) {
     const maxIndex = params.page * params.page_size;
     const sql1 = "SELECT B.*,A.*,DATE_FORMAT(A.order_time,'%Y-%m-%d %H:%i:%s') AS order_time FROM order_job A LEFT JOIN all_job B on A.job_id=B.id WHERE B.uid=" + params.uid +
         " AND A.job_status in ('" + params.status + "') "+orderTimeGeo+
-        " ORDER BY UNIX_TIMESTAMP(now())-UNIX_TIMESTAMP(A.order_time) DESC LIMIT " + minIndex + "," + maxIndex + ";";
+        " ORDER BY "+thisUseSecondsGeo+" DESC LIMIT " + minIndex + "," + maxIndex + ";";
     const sql2 = "SELECT COUNT(1) FROM order_job A LEFT JOIN all_job B on A.job_id=B.id WHERE B.uid=" + params.uid +
         " AND A.job_status in ('" + params.status + "') "+orderTimeGeo+";";
     const sql3 = "SELECT SLEEP(1);";
